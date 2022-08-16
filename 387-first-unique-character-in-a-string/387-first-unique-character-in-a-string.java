9310ae5f77305db3c49a3d03dce258eb7533acc2
Shivanand Jha <90546799+shivanand-jha@users.noreply.github.com>
2022-08-16 22:30:55 +0530

    Time: 23 ms (66.65%), Space: 42.5 MB (96.56%) - LeetHub

---
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int firstUniqChar(String s) {
+    for(char c: s.toCharArray()){
+    if(s.indexOf(c)==s.lastIndexOf(c)) return s.indexOf(c);
+    }
+        return -1;
+    }
+}
